8460267e9f59103aa080548c21e9a5f8cb2328b0
Shubham Goyal <shubham.goyal@nagarro.com>
2021-04-07 09:32:33 +0530

    Initial Commit

---
+++ src/main/java/com/herokutest/testapp297/Testapp297Application.java
@@ -0,0 +1,13 @@
+package com.herokutest.testapp297;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testapp297Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testapp297Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/herokutest/testapp297/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.herokutest.testapp297.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@ResponseBody
+	public String welcomme() {
+		return "Hello Hulalalala";
+	}
+}
